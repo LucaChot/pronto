cbcfd1cb75c72fd7b8d5e84b62dc200f082d1f68
Luca Choteborsky <lc869@cl.cam.ac.uk>
2025-04-05 23:38:40 +0000

    Implemented aggregation server

---
+++ src/aggregate/aggregator.go
@@ -0,0 +1,94 @@
+package aggregate
+
+/*
+Core Components:
+- Aggregator thread
+- gRPC threads
+
+Idea:
+Aggregator thread takes a queue of (U,Sigma)s and performs the merge operation.
+
+The gRPC threads from the remote schedulers read the address of the pointer to
+the current aggregated matrix. It then performs its on merge operation.
+
+
+This method reduces overall latency. We can avoid gRPC requests from queuing.
+In addition, if we did not make gRPC requests queue, merge operations could be lost.
+
+So we use a combination of central thread and gRPC calls to reduce latency while
+ensuring consistency
+
+
+FAULT TOLERANCE:
+We could add fault tolerance by having the remote schedulers use the pod service
+and so when a pod restarts, they are still able to communicate with this pod.
+*/
+
+import (
+	"sync/atomic"
+
+	"gonum.org/v1/gonum/mat"
+
+	log "github.com/sirupsen/logrus"
+    mt "github.com/LucaChot/pronto/src/matrix"
+	pb "github.com/LucaChot/pronto/src/message"
+)
+
+const (
+    MAXWAITING = 20
+    D = 2
+    R = 2
+)
+
+type Aggregator struct {
+    matrices chan *mat.Dense
+    aggregate atomic.Pointer[mat.Dense]
+    pb.UnimplementedAggregateMergeServer
+}
+
+func New() (*Aggregator) {
+    agg := Aggregator {
+        matrices: make(chan *mat.Dense, MAXWAITING),
+    }
+
+    agg.aggregate.Store(mat.NewDense(D, R, nil))
+
+    agg.startAggregateServer()
+
+    return &agg
+}
+
+/*
+TODO: Check whether the remote schedulers expose the matrix with ranks that are
+less than or greater than r?
+I.e. Check if there may be a mismatch in the rank
+*/
+func (agg *Aggregator) Aggregate()  {
+    for {
+        inUSigma := <- agg.matrices
+        _, r := inUSigma.Dims()
+
+        /* Uses sync/atomic pointer */
+        currUSigma := agg.aggregate.Load()
+
+        U, Sigma := mt.AggMerge(currUSigma, inUSigma, r)
+
+        var newUSigma mat.Dense
+        newUSigma.Mul(U, Sigma)
+
+        agg.aggregate.Store(&newUSigma)
+        log.Debug("PERFORMED AGGREGATION")
+    }
+}
+
+/*
+Removed the need to create a new Sigma matrix by passing in the product
+_, inc := inUSigma.Dims()
+inIData := make([]float64, inc)
+for i := range inc {
+    inIData[i] = 1.0
+}
+inI := mat.NewDiagonal(inc, inIData)
+*/
+
+
---
+++ src/aggregate/server.go
@@ -0,0 +1,70 @@
+package aggregate
+
+import (
+	"context"
+	"net"
+
+	pb "github.com/LucaChot/pronto/src/message"
+	log "github.com/sirupsen/logrus"
+	"gonum.org/v1/gonum/mat"
+	"google.golang.org/grpc"
+)
+
+
+
+func (agg *Aggregator) startAggregateServer() {
+    lis, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatalf("failed to serve start server")
+	}
+
+	s := grpc.NewServer()
+    pb.RegisterAggregateMergeServer(s, agg)
+
+	log.WithFields(log.Fields{
+		"ADDRESS": lis.Addr(),
+	}).Debug("STARTED AGGREGATE SERVER")
+
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.WithFields(log.Fields{
+				"ERR": err,
+			}).Fatalf("FAILED TO START SERVER")
+		}
+	}()
+}
+
+/*
+Read the pointer to the struct containing both the U and Sigma
+Uses atomic.Pointer[T] to ensure atomicity
+Research into Golang's memory model
+*/
+func (agg *Aggregator) RequestAggMerge(ctx context.Context, in *pb.DenseMatrix) (*pb.DenseMatrix, error) {
+    log.Debug("RECEIVED AGGREGATE REQUEST")
+    inUSigma := mat.NewDense(int(in.Rows), int(in.Cols), in.Data)
+    agg.matrices<- inUSigma
+
+    aggUSigma := agg.aggregate.Load()
+
+    rows, cols := aggUSigma.Dims()
+    return &pb.DenseMatrix{
+        Rows: int64(rows),
+        Cols: int64(cols),
+        Data: aggUSigma.RawMatrix().Data,
+    }, nil
+}
+
+/*
+Move Merge into Aggregate Server
+U, Sigma = pointer.read()
+
+agg.channel <- (in.U, in.Sigma)
+U', Sigma' := Merge(U, Sigma, in.U, in.Sigma, r)
+
+return &Response {
+    U: U'
+    Sigma: Sigma'
+}, nil
+*/
