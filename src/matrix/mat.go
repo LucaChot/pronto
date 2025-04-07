f23644b29a21afb23675616a6eaf4e175ebba4dd
Luca Choteborsky <lc869@cl.cam.ac.uk>
2025-04-07 23:53:47 +0000

    Implemented matrix functions

---
+++ src/matrix/mat.go
@@ -0,0 +1,187 @@
+package matrix
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+)
+
+// TODO: Add assert statements that ensure the correct values are returned
+/*
+Function Structure
+- Assertions
+- Calculation
+- Handle Subsequent Errors
+*/
+
+
+func SVDR(B mat.Matrix, r int) (*mat.Dense, *mat.DiagDense) {
+    br, bc := B.Dims()
+    if min(br, bc) < r {
+		panic(fmt.Errorf("rank r must be smaller than the dimensions of matrix"))
+	}
+
+    var svd mat.SVD
+    svd.Factorize(B, mat.SVDThinU)
+
+    var FullU, U *mat.Dense
+    svd.UTo(FullU)
+    m, _ := FullU.Dims()
+    U.Copy(FullU.Slice(0, m, 0, r))
+
+    var Sigma *mat.DiagDense
+    sigma := svd.Values(nil)
+    Sigma = mat.NewDiagDense(r, sigma[:r])
+
+    return U, Sigma
+}
+
+/* Concatenates two matrices A and B */
+func Concatenate(A, B *mat.Dense) (*mat.Dense) {
+    ar, ac := A.Dims()
+    br, bc := B.Dims()
+
+    if ar != br {
+		panic(fmt.Errorf("input matrices must have the same number of rows"))
+	}
+
+    /* Creates matrix storing the concatenation */
+    C := mat.NewDense(ar, ac + bc, nil)
+
+    /* Use of raw matrices to access slices */
+    aRaw := A.RawMatrix()
+    bRaw := B.RawMatrix()
+    cRaw := C.RawMatrix()
+
+    /* For each row copy the slices of A and B into that of C */
+    for i := range ar {
+        destCRowSlice := cRaw.Data[i*cRaw.Stride : i*cRaw.Stride + ac + bc]
+        srcARowSlice := aRaw.Data[i*aRaw.Stride : i*aRaw.Stride + ac]
+        srcBRowSlice := bRaw.Data[i*bRaw.Stride : i*bRaw.Stride + bc]
+
+        copy(destCRowSlice[:ac], srcARowSlice)
+        copy(destCRowSlice[ac:], srcBRowSlice)
+    }
+
+    return C
+}
+
+func Merge(U1 mat.Matrix, Sigma1 mat.Matrix, U2 mat.Matrix, Sigma2 mat.Matrix, r int, forget float64, enhance float64) (*mat.Dense, *mat.DiagDense) {
+    /*
+    Z = U_1.transpose() * U_2
+    Q, R = QR(U_2 - (U_1 * Z))
+
+    U', Sigma'' = SVD( [[Sigma_1, Z * Sigma_2], [0, R * Sigma_2]], r)
+
+    U'' = [U_1, Q]U'
+    */
+    var temp1 *mat.Dense
+    temp1.Mul(U1, Sigma1)
+    temp1.Scale(forget, temp1)
+
+    var temp2 *mat.Dense
+    temp2.Mul(U2, Sigma2)
+    temp2.Scale(enhance, temp2)
+
+    concat := Concatenate(temp1, temp2)
+
+    return SVDR(concat, r)
+}
+
+func ImpactOfRank(Sigma *mat.DiagDense, r int) (float64) {
+    if Sigma == nil {
+		panic(fmt.Errorf("input matrix Sigma is nil"))
+	}
+    sr, _ := Sigma.Dims()
+    if r < 1 || sr < r {
+		panic(fmt.Errorf("rank is out of bounds"))
+    }
+
+    diagData := Sigma.RawBand().Data
+    var total float64
+    for i := range r {
+        total += diagData[i]
+    }
+
+    if total == 0 {
+        if diagData[r-1] == 0 {
+            return 0
+        }
+		panic(fmt.Errorf("division by zero: sum of first %d diagonal elements is zero", r))
+    }
+
+    return diagData[r-1] / total
+}
+
+func Rank(inU *mat.Dense, inSigma *mat.DiagDense, r int, alpha, beta float64) (*mat.Dense, *mat.DiagDense) {
+    /*
+    total_variance = sum(variance(Sigma,i))
+    Epsilon = variance(Sigma, r) / total_variance
+
+    if Epsilon < alpha:
+        U[:r-1], Sigma[:r-1]
+
+    if Epsilon > beta:
+        e = canonical(r+1)
+        [U,e], Sigma[:r+1]
+    */
+    var outU *mat.Dense
+    var outSigma *mat.DiagDense
+
+    ur, uc := inU.Dims()
+    _, sc := inU.Dims()
+
+    // Assume r < U.Cols(), Sigma.Cols()
+    if uc <= r || sc <= r {
+        panic(fmt.Errorf("r must be smaller than the number of columns in U"))
+    }
+
+    impact := ImpactOfRank(inSigma, r)
+    var rank int
+    if impact < alpha {
+        rank = r-1
+    } else if impact <= beta {
+        rank = r
+    } else {
+        /* TODO: Complete with understanding from Andreas */
+        rank = r+1
+    }
+    outU.Copy(inU.Slice(0, ur, 0, rank))
+
+    outDiag := make([]float64, rank)
+    copy(outDiag, inSigma.RawBand().Data[:rank])
+    outSigma = mat.NewDiagDense(rank, outDiag)
+
+
+
+    return outU, outSigma
+}
+
+func AggMerge(USigma1 *mat.Dense, USigma2 *mat.Dense, r int) (*mat.Dense, *mat.DiagDense) {
+    /*
+    Z = U_1.transpose() * U_2
+    Q, R = QR(U_2 - (U_1 * Z))
+
+    U', Sigma'' = SVD( [[Sigma_1, Z * Sigma_2], [0, R * Sigma_2]], r)
+
+    U'' = [U_1, Q]U'
+
+    Different Version:
+    var Z *mat.Dense
+    Z.Mul(U_1.T(), U_2)
+
+
+    var MultU1AndZ, SubtractU2AndMult *mat.Dense
+    MultU1AndZ.Mul(U_1, Z)
+    SubtractU2AndMult.Sub(U_2, MultU1AndZ)
+
+    var qr mat.QR
+    var Q, R *mat.Dense
+    qr.Factorize(SubtractU2AndMult)
+    qr.QTo(Q)
+    qr.RTo(R)
+    */
+    concat := Concatenate(USigma1, USigma2)
+
+    return SVDR(concat, r)
+
+}
--- src/metrics/localfpca.go
+++ src/metrics/localfpca.go
@@ -3,22 +3,37 @@ package metrics
 import (
 	log "github.com/sirupsen/logrus"
 
+   "gonum.org/v1/gonum/mat"
 	linuxproc "github.com/c9s/goprocinfo/linux"
+    mt "github.com/LucaChot/pronto/src/matrix"
 )
 
+const BatchSize = 10
+const r = 10
+
 type LocalFPCAAgent struct {
-	cpu  linuxproc.Stat
-	mem  linuxproc.MemInfo
-	disk []linuxproc.DiskStat
-	net  []linuxproc.NetworkStat
+	cpu         linuxproc.Stat
+	mem         linuxproc.MemInfo
+	disk        []linuxproc.DiskStat
+	net         []linuxproc.NetworkStat
+    U           *mat.Dense
+    lastU           *mat.Dense
+    Sigma       *mat.DiagDense
+    B           *mat.Dense
+    localU      *mat.Dense
+    localSigma  *mat.DiagDense
+    r           int
+    forget      float64
+    enhance     float64
+    alpha       float64
+    beta        float64
+    epsilon     float64
 }
 
 func New() LocalFPCAAgent {
-	mc := LocalFPCAAgent{}
-
-    // mc.U = 0
-    // mc.Sigma = 0
-    // mc.B = []
+	mc := LocalFPCAAgent{
+        r: r,
+    }
 
 	go mc.RunLocalUpdates()
 
@@ -29,12 +44,23 @@ func (mc *LocalFPCAAgent) RunLocalUpdates() {
     for {
         mc.Collect()
 
-        // If len(mc.B) = b
-		//mc.FPCAEdge()
+        if _, c := mc.B.Dims(); c == BatchSize {
+		    mc.FPCAEdge()
+            mc.B.Reset()
+        }
 
+        if mat.EqualApprox(mc.U, mc.lastU, mc.epsilon) {
+            //mc.RequestU
+        }
+        mc.U = mc.lastU
     }
 }
 
+/*
+TODO: Ask andreas about the pseudocode of the paper, in the rank function, it assumes
+that the sigma is of size r x r, so what does Sigma_[r+1] do?
+TODO: Ask which version of Merge should I implement
+*/
 func (mc *LocalFPCAAgent) FPCAEdge() {
     /*
     * Update embedding estimates *
@@ -48,31 +74,19 @@ func (mc *LocalFPCAAgent) FPCAEdge() {
 
     * Adjust the rank based on information it has seen so far *
     mc.GlobalU, mc.GlobalSigma := Rank(mc.GlobalU, mc.GlobalSigma, alpha, beta, r)
-
     */
+
+    if mc.localU == nil && mc.localSigma == nil {
+        mc.localU, mc.localSigma = mt.SVDR(mc.B, mc.r)
+    } else {
+        _, bc := mc.B.Dims()
+        identity := mat.NewDiagDense(bc, nil)
+        mc.localU, mc.localSigma = mt.Merge(mc.localU, mc.localSigma, mc.B, identity, mc.r, mc.enhance, mc.forget)
+    }
+
+    /* Pass in rank r+1 so that we can increase the rank in the next step */
+    tempU, tempSigma := mt.AggMerge(mc.U, mc.Sigma, mc.localU, mc.localSigma, mc.r)
+
+    mc.U, mc.Sigma = mt.Rank(tempU, tempSigma, mc.r, mc.alpha, mc.beta)
 }
 
-func (mc *LocalFPCAAgent) Merge() {
-    /*
-    Z = U_1.transpose() * U_2
-    Q, R = QR(U_2 - (U_1 * Z))
-
-    U', Sigma'' = SVD( [[Sigma_1, Z * Sigma_2], [0, R * Sigma_2]], r)
-
-    U'' = [U_1, Q]U'
-    */
-}
-
-func (mc *LocalFPCAAgent) Rank() {
-    /*
-    total_variance = sum(variance(Sigma,i))
-    Epsilon = variance(Sigma, r) / total_variance
-
-    if Epsilon < alpha:
-        U[:r-1], Sigma[:r-1]
-
-    if Epsilon > beta:
-        e = canonical(r+1)
-        [U,e], Sigma[:r+1]
-    */
-}
