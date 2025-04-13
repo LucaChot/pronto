7f98bf3877e7cd1dc7463ba90306f570cfd592a7
Luca Choteborsky <lc869@cl.cam.ac.uk>
2025-04-13 00:22:23 +0000

    Fixed central scheduler restart
    Fixed the restarting of the central scheduler due to the closing of the
    watch API connection. Fix involves moving loop into another
    non-terminating for loop.

--- src/central/central_sched.go
+++ src/central/central_sched.go
@@ -90,50 +90,52 @@ func (ctl *CentralScheduler) findNode() string {
 /* Core Scheduling loop */
 func (ctl *CentralScheduler) Schedule() {
 
-    /* Creates a watch interface for all pods that use this scheduler */
-	watch, _ := ctl.clientset.CoreV1().Pods("").Watch(context.TODO(), metav1.ListOptions{
-		FieldSelector: fmt.Sprintf("spec.schedulerName=%s,spec.nodeName=", ctl.Name),
-	})
+    for {
+        /* Creates a watch interface for all pods that use this scheduler */
+        watch, _ := ctl.clientset.CoreV1().Pods("").Watch(context.TODO(), metav1.ListOptions{
+            FieldSelector: fmt.Sprintf("spec.schedulerName=%s,spec.nodeName=", ctl.Name),
+        })
 
-    /* Loops over all new pod events we detect */
-	for event := range watch.ResultChan() {
-        /* Ignore events where pods have been added */
-		if event.Type != "ADDED" {
-			continue
-		}
+        /* Loops over all new pod events we detect */
+        for event := range watch.ResultChan() {
+            /* Ignore events where pods have been added */
+            if event.Type != "ADDED" {
+                continue
+            }
 
-        start := time.Now().UTC()
+            start := time.Now().UTC()
 
-		p := event.Object.(*v1.Pod)
-		log.WithFields(log.Fields{
-			"namespace": p.Namespace,
-			"pod":       p.Name,
-		}).Debug("BEGIN POD SCHEDULE")
-
-
-        /* Find a node to place the pod */
-        node := ctl.findNode()
-        if node == "" {
-            log.Debug("FAILED TO FIND SUITABLE NODE")
-            continue
-        }
-
-        ctl.placePodToNode(p, node)
-
-        /* Collect information for event */
-		end := time.Now().UTC()
-        nanosecondsSpent := end.Sub(start).Nanoseconds()
-        annotations := map[string]string{
-            "scheduler/nanoseconds": fmt.Sprintf("%d", nanosecondsSpent),
-        }
-
-        /* Creates a new event alerting the binding of the pod */
-        err := ctl.createSchedEvent(p, node, end, annotations)
-        if err != nil {
+            p := event.Object.(*v1.Pod)
             log.WithFields(log.Fields{
-                "err": err,
-            }).Debug("FAILED TO CREATE EVENT")
+                "namespace": p.Namespace,
+                "pod":       p.Name,
+            }).Debug("BEGIN POD SCHEDULE")
+
+
+            /* Find a node to place the pod */
+            node := ctl.findNode()
+            if node == "" {
+                log.Debug("FAILED TO FIND SUITABLE NODE")
+                continue
+            }
+
+            ctl.placePodToNode(p, node)
+
+            /* Collect information for event */
+            end := time.Now().UTC()
+            nanosecondsSpent := end.Sub(start).Nanoseconds()
+            annotations := map[string]string{
+                "scheduler/nanoseconds": fmt.Sprintf("%d", nanosecondsSpent),
+            }
+
+            /* Creates a new event alerting the binding of the pod */
+            err := ctl.createSchedEvent(p, node, end, annotations)
+            if err != nil {
+                log.WithFields(log.Fields{
+                    "err": err,
+                }).Debug("FAILED TO CREATE EVENT")
+            }
         }
-	}
+    }
 }
 
